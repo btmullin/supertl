fd41a2c0230eb51bccdca41419ab3b9d429154cc
Ben Mullin <benjamin.t.mullin@gmail.com>
2023-03-25 19:18:16 -0500

    Doc and clean-up

--- supertl/ui/ActivityMapWidget.py
+++ supertl/ui/ActivityMapWidget.py
@@ -5,36 +5,49 @@ from supertl.Data import Activity
 import io
 
 class ActivityMapWidget(QWebEngineView):
+    """
+    A widget that will visually display the gps route of an activity.
+
+    Args:
+        QWebEngineView (_type_): _description_
+    """
+
     def __init__(self):
         super().__init__()
 
-        self.setHtml('<H1>Nothing Yet</H1>')
-        #f = open('c:/git/supertl/devplayground/map.html','r')
-        #self.setHtml(f.read())
+        self.setHtml('<H1 style="padding:100px;"><CENTER>No Activity Selected</CENTER></H1>')
 
     def set_activity(self, activity: Activity):
+        """
+        Set the activity to be displayed by the Activity Map and redraw the map.
+
+        Args:
+            activity (Activity): The Activity to display.
+        """
         points = list(zip(activity.gps_data.time_series_data['position_lat'],
                           activity.gps_data.time_series_data['position_long']))
 
-        map = folium.Map()
+        f_map = folium.Map()
 
         # Add start and end markers
         folium.Marker(points[0],
-                      icon=folium.Icon(color="green", icon="circle-play", prefix="fa")).add_to(map)
+                      icon=folium.Icon(color="green",
+                                       icon="circle-play",
+                                       prefix="fa")).add_to(f_map)
         folium.Marker(points[-1],
-                      icon=folium.Icon(color="red", icon="circle-stop", prefix="fa")).add_to(map)
+                      icon=folium.Icon(color="red", icon="circle-stop", prefix="fa")).add_to(f_map)
 
         # plot all lines
-        folium.PolyLine(points, weight=4, opacity=1).add_to(map)
+        folium.PolyLine(points, weight=4, opacity=1).add_to(f_map)
 
         # zoom to the activity
-        sw = [activity.gps_data.time_series_data['position_lat'].min(),
-              activity.gps_data.time_series_data['position_long'].min()]
-        ne = [activity.gps_data.time_series_data['position_lat'].max(),
-              activity.gps_data.time_series_data['position_long'].max()]
-        map.fit_bounds([sw,ne])
+        sw_limit = [activity.gps_data.time_series_data['position_lat'].min(),
+                    activity.gps_data.time_series_data['position_long'].min()]
+        ne_limit = [activity.gps_data.time_series_data['position_lat'].max(),
+                    activity.gps_data.time_series_data['position_long'].max()]
+        f_map.fit_bounds([sw_limit, ne_limit])
 
         # update the map
         data = io.BytesIO()
-        map.save(data, close_file=False)
+        f_map.save(data, close_file=False)
         self.setHtml(data.getvalue().decode())
