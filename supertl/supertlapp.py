73f75c052a25ccce3b6048bd9e84b98bcc0f8543
Ben Mullin <benjamin.t.mullin@gmail.com>
2023-03-25 19:18:39 -0500

    Styling and clean-up

--- supertl/supertlapp.py
+++ supertl/supertlapp.py
@@ -1,6 +1,5 @@
 import sys
 from PySide6 import QtCore, QtWidgets
-from PySide6.QtWebEngineWidgets import QWebEngineView
 from supertl.ui import WorkoutCalendar, ActivityMapWidget
 from supertl.Data import Activity
 
@@ -8,9 +7,16 @@ class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.select_activity_button = QtWidgets.QPushButton("Select activity file")
-
         self.layout = QtWidgets.QGridLayout(self)
+
+
+        self.select_activity_button = QtWidgets.QPushButton("Select activity file")
+        self.select_activity_button.setStyleSheet("border: 5px solid '#555555';" +
+                                                  "border-radius: 15px;" +
+                                                  "padding: 15px 0px;" +
+                                                  "margin: 10px 30px;" +
+                                                  "font-size: 20px;")
+        self.select_activity_button.clicked.connect(self.magic)
         self.layout.addWidget(self.select_activity_button,1,0)
 
         self.workout_calendar = WorkoutCalendar.WorkoutCalendar()
@@ -19,15 +25,19 @@ class MyWidget(QtWidgets.QWidget):
         self.selected_file = ""
         self.selected_file_text = QtWidgets.QLabel("No File",
                                      alignment=QtCore.Qt.AlignCenter)
+        self.selected_file_text.setStyleSheet("font-size: 20px;")
         self.layout.addWidget(self.selected_file_text,1,1)
 
         self.map_view = ActivityMapWidget.ActivityMapWidget()
+        # this doesn't work
+        # self.map_view.setStyleSheet("margin: 0px 5px 10px 0px;" +
+        #                             "border: 10px solid '#FF0000';"+
+        #                             "padding: 200px 100px;")
         self.layout.addWidget(self.map_view, 0,1)
 
         # place to render a graph
         # place to display the workout summary
 
-        self.select_activity_button.clicked.connect(self.magic)
 
     @QtCore.Slot()
     def magic(self):
@@ -43,9 +53,12 @@ class MyWidget(QtWidgets.QWidget):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
+    screen_rect = app.primaryScreen().geometry()
+    inset = 30
 
     widget = MyWidget()
-    widget.resize(800, 600)
+    widget.resize(screen_rect.width()-inset*2, screen_rect.height()-inset*2-80)
+    widget.move(screen_rect.top()+inset, screen_rect.left()+inset)
     widget.show()
 
     sys.exit(app.exec())
